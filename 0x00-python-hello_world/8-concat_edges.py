05a8f408fb7d4aeea97ac18207410aee69c2722f
Horac3 <lwandahorace@gmail.com>
2023-06-06 07:09:06 -0700

    Finished up the remaining excercies minus 1

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+text = "Python is an interpreted, interactive, object-oriented programming" \
+       " language that combines remarkable power with very clear syntax"
+result = text[39:67] + text[107:112] + text[:6]
+print(result)
