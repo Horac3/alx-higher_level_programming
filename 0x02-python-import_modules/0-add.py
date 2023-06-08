17e746860b9f3e4cddfd34fe38159572b4a042ac
Horac3 <lwandahorace@gmail.com>
2023-06-08 18:26:17 +0200

    0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-"""Print the sum, difference, multiple and quotient of 10 and 5."""
-from add_0 import add
 
-"""Define variables"""
-a = 1
-b = 2
+if __name__ == "__main__":
+    """Print the sum, difference, multiple and quotient of 10 and 5."""
+    from add_0 import add
 
-"""Call the add function and print the result""" 
-print("{} + {} = {}".format(a, b, add(a, b)))
+    """Define variables"""
+    a = 1
+    b = 2
+
+    """Call the add function and print the result""" 
+    print("{} + {} = {}".format(a, b, add(a, b)))
