1a8c13d60625a350ae0cfff6b000857e242e8a1d
Horac3 <lwandahorace@gmail.com>
2023-06-08 18:45:22 +0200

    2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 import sys
 
-# Get the command-line arguments
-arguments = sys.argv[1:]
+if __name__ == "__main__":
+    # Get the command-line arguments
+    arguments = sys.argv[1:]
 
-# Print the number of arguments
-num_arguments = len(arguments)
-print("Number of argument(s):", num_arguments)
+    # Print the number of arguments
+    num_arguments = len(arguments)
+    print("Number of argument(s):", num_arguments)
 
-# Print the list of arguments or a dot if no arguments were passed
-if num_arguments > 0:
-    print("Arguments:")
-    for i, arg in enumerate(arguments, start=1):
-        print("{}: {}".format(i, arg))
-else:
-    print(".")
+    # Print the list of arguments or a dot if no arguments were passed
+    if num_arguments > 0:
+        print("Arguments:")
+        for i, arg in enumerate(arguments, start=1):
+            print("{}: {}".format(i, arg))
+    else:
+        print(".")
