8965fcbff0df0923bf4346bb99cf3cc89519b79e
Horac3 <lwandahorace@gmail.com>
2023-06-11 21:43:27 +0200

    Fixed requirements

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -5,8 +5,8 @@ def add_tuple(tuple_a=(), tuple_b=()):
         tuple_a += (0,) * (2 - len(tuple_a))
     if len(tuple_b) < 2:
         tuple_b += (0,) * (2 - len(tuple_b))
-    
+
     sum_0 = tuple_a[0] + tuple_b[0]
     sum_1 = tuple_a[1] + tuple_b[1]
-    
+
     return sum_0, sum_1
